5f1739d93274b8214e5254a945b99f00bed5cb0c
sokomishalov <sokomishalov@mail.ru>
2020-01-12 15:24:56 +0300

    further progress

--- commons_flutter/lib/src/containers/debug_widget.dart
+++ commons_flutter/lib/src/containers/debug_widget.dart
@@ -1,13 +1,20 @@
 import 'package:commons_dart/commons_dart.dart';
+import 'package:commons_flutter/commons_flutter.dart';
 import 'package:flutter/material.dart';
 
 class DebugWidget extends StatelessWidget {
-  const DebugWidget({Key key, this.child}) : super(key: key);
+  const DebugWidget({
+    Key key,
+    @required this.child,
+  }) : super(key: key);
 
   final Widget child;
 
   @override
   Widget build(BuildContext context) {
-    return isDebugModeOn() ? child : Container();
+    return ConditionalWidget(
+      predicate: () => isDebugModeOn(),
+      child: child,
+    );
   }
 }
